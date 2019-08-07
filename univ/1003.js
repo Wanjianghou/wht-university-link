db054f52cc3b23453fa17a53ff4699d5b41bc06f
Wanjianghou <51628829+Wanjianghou@users.noreply.github.com>
2019-08-07 17:11:37 +0800

    中国人民大学
    wwwwww

---
+++ univ/1003.js
@@ -0,0 +1,26 @@
+var univ_list = {
+    school: "中国人民大学",
+    id: "1003",
+    link: [ {
+        name: "人大主页", 
+        url: "http://www.ruc.edu.cn/" 
+    }, {
+        name: "微人大", 
+        url: "http://v.ruc.edu.cn/"
+    }, {
+        name:"教务处",
+        url:"http://jiaowu.ruc.edu.cn/"
+    }, {
+        name:"研究生院"
+        url:"http://grs.ruc.edu.cn/"
+    }, {
+        name:"图书馆"
+        url:"http://www.lib.ruc.edu.cn/"
+    }, {
+        name:"人大邮箱"
+        url:"http://mail.ruc.edu.cn/"
+    }, {
+        name:"微批恩"
+        url:"http://vpn.ruc.edu.cn/"
+    } ]
+};
